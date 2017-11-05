d83337118ee8df04c0d61a58240ddc9ec3426077
Abhi9024 <gr.abishek.gr@gmail.com>
2017-11-06 00:48:35 +0530

    With reactive forms

--- FootballWorld/ClientApp/app/components/football/football.component.ts
+++ FootballWorld/ClientApp/app/components/football/football.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component,OnInit } from '@angular/core';
 import { IClub } from '../../interface/IClub';
 import { FootballService } from '../../service/football.service';
+import {  FormGroup, FormBuilder, Validators } from '@angular/forms';
  
 @Component({
     selector: 'football',
@@ -10,15 +11,48 @@ import { FootballService } from '../../service/football.service';
 export class FootballComponent {
 
     clubs: IClub[];
+    clubsForm: FormGroup;
+    IsAddClubEnabled: boolean;
 
-    constructor(private footBallService: FootballService) { }
+    club: IClub = {
+        clubName: '',
+        clubDescription: '',
+        championsLeagueTitle:0,
+        leagueName: '',
+        leagueTitles: 0,
+        clubWorth: '',
+        country: '',
+        ranking: 0,
+        state:''
+    };
+
+    constructor(private footBallService: FootballService, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this.getChampionsLeagueClubs();
+        this.buildForm();
     };
 
     getChampionsLeagueClubs() {
         this.footBallService.getChampionsLeagueClubs()
             .subscribe(result => { this.clubs = result }, (err) => console.log(err));
-    }
+    };
+
+    buildForm() {
+        this.clubsForm = this.formBuilder.group({
+            clubName: [this.club.clubName, Validators.required],
+            clubDescription: [this.club.clubDescription, Validators.required],
+            championsLeagueTitle: [this.club.championsLeagueTitle, Validators.required],
+            leagueName: [this.club.leagueName, Validators.required],
+            leagueTitles: [this.club.leagueTitles, Validators.required],
+            clubWorth: [this.club.clubWorth, Validators.required],
+            country: [this.club.country, Validators.required],
+            ranking: [this.club.ranking, Validators.required],
+            state: [this.club.state, Validators.required]
+        });
+    };
+
+    toggleAddClub(): void {
+        this.IsAddClubEnabled = (this.IsAddClubEnabled) ? false : true;
+    };
 }
